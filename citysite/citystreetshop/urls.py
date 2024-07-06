71dfe645c36bb51cb5a38febc7d6e33c772bb6db
vitalikx100 <vitalikstarigin@mail.ru>
2024-07-06 20:05:49 +0400

    Added list of shop by parametres

--- citysite/citystreetshop/urls.py
+++ citysite/citystreetshop/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.city_all, name='cities'),
     path('shop/', views.shop_create, name='shop_create'),
+    path('shops/', views.shop_view, name='shops_view'),
     path('<int:pk>/street', views.CityStreet.as_view(), name='city-street')
 ]
--- citysite/citystreetshop/views.py
+++ citysite/citystreetshop/views.py
@@ -31,3 +31,19 @@ def shop_create(request):
     }
 
     return render(request, 'citystreetshop/shop_create.html', data)
+
+def shop_view(request):
+    street_id = request.GET.get('street')
+    city_id = request.GET.get('city')
+
+    shops = Shop.objects.all()
+    if city_id:
+        shops = shops.filter(city_id=city_id)
+    if street_id:
+        shops = shops.filter(street_id=street_id)
+
+    data = {
+        'shops': shops
+    }
+
+    return render(request, 'citystreetshop/shops_view.html', data)
